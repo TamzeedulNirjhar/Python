6317b4be8852d0fc6e8d4c809bac827faa0086b8
TamzeedulNirjhar <aadheen.jhil@gmail.com>
2026-03-15 20:54:22 +0600

    name input

--- Module1/Lesson1/Activity.py
+++ Module1/Lesson1/Activity.py
@@ -2,3 +2,6 @@
 print("Hello world")
 print("My name is Nirjhar")
 print("This is my first python class")
+print("my first lesson")
+print("this is my first class on python")
+print("my sister's name is Arshia Armin  ")
---
+++ Module1/Lesson2/Activity.py
@@ -0,0 +1,9 @@
+x = 5
+y = "Jhil"
+
+print(x)
+print(y)
+
+name = input("Enter your name:")
+
+print("Hello,", name, "Welcome to python")
