ed036090cdf2bd0a557cac38a6fac2847b8b3414
TamzeedulNirjhar <aadheen.jhil@gmail.com>
2026-03-20 22:19:33 +0600

    birthday

---
+++ Module1/Acp1/Activity.py
@@ -0,0 +1,11 @@
+Jhil = "December 7"
+Alvi = "January 1"
+Sara = "January 16"
+Pranon = "April 29"
+Asan = "November 23"
+
+print(Jhil)
+print(Alvi)
+print(Sara)
+print(Pranon)
+print(Asan)
--- Module1/Lesson3/Activity2.py
+++ Module1/Lesson3/Activity2.py
@@ -4,5 +4,5 @@ print(len(str1))
 print(str1[1])
 print(str1[0:3])
 str3 = str1 + str2
-print(str3)
+print(str3) 
 print(str2[::-1])
