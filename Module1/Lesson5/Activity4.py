84618366d0fa897bb3ea96d3b611b46ad8043f54
TamzeedulNirjhar <aadheen.jhil@gmail.com>
2026-04-05 21:11:53 +0600

    small or great

---
+++ Module1/Lesson5/Activity4.py
@@ -0,0 +1,9 @@
+i = int(input("Please enter a number"))
+if (i<15):
+    print("i is smaller than 15")
+    print("i'm is in if block")
+else:
+    print("i is bigger than 15")
+    print("i'm in else block")
+
+print("i am not in else or if block")
