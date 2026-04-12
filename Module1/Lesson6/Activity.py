92dc9b62c92b6e0cd0b73ecf97bce3ebe68ea934
TamzeedulNirjhar <aadheen.jhil@gmail.com>
2026-04-12 20:53:50 +0600

    true and false

---
+++ Module1/Lesson6/Activity.py
@@ -0,0 +1,11 @@
+a = 12
+b = -12
+c = 0
+if a>1 and b>1 and c>-1:
+    print("All of them are positive")
+elif a>1 or b>1 or c>-1:
+    print("Either of them are true")
+elif c!=0:
+    print("C is not zero ")
+else:
+    print("All them are wrong")
