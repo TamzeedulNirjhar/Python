70d02286bfb8f281ab94e1794d45f5ef94e6df20
TamzeedulNirjhar <aadheen.jhil@gmail.com>
2026-05-09 00:19:41 +0600

    circumference

--- Module3/Acp1/Activity.py
+++ Module3/Acp1/Activity.py
@@ -3,6 +3,6 @@ square = turtle.Turtle()
 
 for i in range(4):
     square.forward(100)
-    square.right(90) 
+    square.right(90)
 
 turtle.done()
---
+++ Module3/Acp2/Activity.py
@@ -0,0 +1,6 @@
+def circumference(radius):
+    return 2 * 3.14 * radius
+
+r = int(input("Enter radius: "))
+
+print("Circumference =", circumference(r))
